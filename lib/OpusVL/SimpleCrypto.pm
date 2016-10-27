3ae1534193e0ae55a17ffcdf4dd36e2a055537e5
Colin Newell <colin@opusvl.com>
2016-10-27 17:35:19 +0100

    Filled in pod

--- lib/OpusVL/SimpleCrypto.pm
+++ lib/OpusVL/SimpleCrypto.pm
@@ -56,6 +56,8 @@ OpusVL::SimpleCrypto - Very simple encyption methods.
 
 =head1 DESCRIPTION
 
+Simple encrypt and decrypt methods.
+
     my $s = OpusVL::SimpleCrypto->GenerateKey;
     print $s->key_string, "\n";
     my $ct = $s->encrypt('Test');
@@ -63,21 +65,34 @@ OpusVL::SimpleCrypto - Very simple encyption methods.
     my $crypto = OpusVL::SimpleCrypto->new({ key_string => $key_string });
     my $message = $crypto->decrypt($ct);
 
-
+This uses Crypt::Sodium under the hood to do simple symetric (authenticated)
+encryption and decryption.
 
 =head1 METHODS
 
 =head2 GenerateKey
 
+Create a key and return new OpusVL::SimpleCrypto initialized with it.
+
+Use the key_string method to get the key out in a format useful for storing.
+
 =head2 encrypt
 
+Encrypt text.
+
 =head2 decrypt
+
+Decrypt text.  Note that ciphertext that has been meddled with will
+not decrypt, and the function will return undef instead.
 
 =head1 ATTRIBUTES
 
 =head2 key_string
 
+The key in a text friendly format.
+
 =head2 key
 
+The key in binary.
 
 =cut
