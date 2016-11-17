8e8ee9d04cdd2a53378bd5e27edb9570d01d35a7
Colin Newell <colin@opusvl.com>
2016-11-17 13:56:57 +0000

    Bumped version number

--- lib/OpusVL/SimpleCrypto.pm
+++ lib/OpusVL/SimpleCrypto.pm
@@ -8,7 +8,7 @@ use Moo;
 use Crypt::Sodium;
 use MIME::Base64;
 
-our $VERSION = '0.008';
+our $VERSION = '0.009';
 
 has key_string => (is => 'rw', lazy => 1, builder => '_build_key_string');
 has key => (is => 'ro', lazy => 1, builder => '_build_key');
